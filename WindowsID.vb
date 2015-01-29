4e82ff86b05fd738b4fb19e6787cb304a870d396
Nadeem Ramjan <nadeemramjan@gmail.com>
2015-01-29 08:58:22 -0500

    Update WindowsID.vb

--- WindowsID.vb
+++ WindowsID.vb
@@ -1,5 +1,8 @@
 'The following will extract the WindowsUserName and can be used for verification
 
+Private Declare Function GetUserName Lib "advapi32.dll" _
+Alias "GetUserNameA" (ByVal lpBuffer As String, nSize As Long) As Long
+
 Public Function WindowsUser() As String
   Dim strUsername As String
   strUsername = String(100, vbNullChar)
