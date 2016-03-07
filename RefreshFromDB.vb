1f0ddd251af9d5c6b915e64c2b14369fc0a9ac3b
Nadeem Ramjan <nadeemramjan@gmail.com>
2016-03-07 10:23:46 -0500

    Update RefreshFromDB.vb

--- RefreshFromDB.vb
+++ RefreshFromDB.vb
@@ -1,6 +1,19 @@
 Sub RefreshData()
     With ActiveWorkbook.Connections("ConnectionName").OLEDBConnection
-      .Connection = "OLEDB;Provider=SQLOLEDB.1;Persist Security Info=True;Initial Catalog=Metron;Data Source=DBSERVERPATH;Extended Properties=""Trusted_connection=false;Use Procedure for Prepare=1;Auto Translate=True;Packet Size=4096;Workstation ID=LNNYPBGR84E1FF;Use Encryption for Data=False;Tag with column collation when possible=False;UID=Username;PWD=password;Use Procedure for Prepare=1;Auto Translate=True;Packet Size=4096;Workstation ID=LNNYPBGR84E1FF;Use Encryption for Data=False;Tag with column collation when possible=False;"";Use Procedure for Prepare=1;Auto Translate=True;Packet Size=4096;Workstation ID=LNNYPBGR84E1FF;Use Encryption for Data=False;Tag with column collation when possible=False"
+      .Connection = "OLEDB;" & _
+                    "Provider=SQLOLEDB.1;" & _
+                    "Persist Security Info=True; " & _
+                    "Initial Catalog=DatabaseName; " & _
+                    "Data Source=ServerName; " & _
+                    "Trusted_connection=true; " & _
+                    "Use Procedure for Prepare=1; " & _
+                    "Auto Translate=True; " & _
+                    "Packet Size=4096; " & _
+                    "Use Encryption for Data=False; " & _
+                    "Tag with column collation when possible=False; " & _
+                    "Use Procedure for Prepare=1; " & _
+                    "Auto Translate=True; "
+                    
       .CommandText = "select * from Table"
       
       .BackgroundQuery = False
